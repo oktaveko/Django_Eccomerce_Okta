1c874aa75f0c59acd758fb4add2448ec80f2e04f
oktaveko <fransokta77@gmail.com>
2022-07-03 21:17:53 +0700

    setting redis, database, smtp for production

--- storefront/settings/common.py
+++ storefront/settings/common.py
@@ -164,17 +164,12 @@ SIMPLE_JWT = {
 }
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'localhost'
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''
-EMAIL_PORT = 2525
-DEFAULT_FROM_EMAIL ='from@oktavsell.com'
 
 ADMINS = [
     ('Oktav', 'admin@oktavsell.com')
 ]
 
-CELERY_BROKER_URL = 'redis://localhost:6379/1'
+
 CELERY_BEAT_SCHEDULE = {
     'notify_customers': {
         'task': 'playground.tasks.notify_customers',
@@ -183,16 +178,6 @@ CELERY_BEAT_SCHEDULE = {
     }
 }
 
-CACHES = {
-    "default": {
-        "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/2",
-        "TIMEOUT" : 10 * 60, 
-        "OPTIONS": {
-            "CLIENT_CLASS": "django_redis.client.DefaultClient",
-        }
-    }
-}
 
 LOGGING = {
     'version': 1,
--- storefront/settings/prod.py
+++ storefront/settings/prod.py
@@ -1,6 +1,4 @@
 import os
-
-from storefront.settings.dev import SECRET_KEY
 from .common import *
 
 DEBUG = False
@@ -8,3 +6,39 @@ DEBUG = False
 SECRET_KEY = os.environ['SECRET_KEY']
 
 ALLOWED_HOSTS = ['django-eccomerce-okta.herokuapp.com']
+
+DATABASES = {
+    'default':{
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get("DB_NAME"),
+        'USER' : os.environ.get("DB_USER"),
+        'PASSWORD':os.environ.get("DB_PASS"),
+        'HOST' :os.environ.get("DB_HOST"),
+        'PORT' : '3306',
+        'OPTIONS' : {
+            #'sql_mode' : 'traditional',
+        }
+    }
+}
+
+REDIS_URL = os.environ.get('REDIS_URL')
+
+CELERY_BROKER_URL = REDIS_URL
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": REDIS_URL,
+        "TIMEOUT" : 10 * 60, 
+        "OPTIONS": {
+            'PASSWORD': os.environ.get('REDIS_PASS'),
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = 465
+
